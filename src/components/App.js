c3bd9b8d651d970c9c8b9f32831084866926226b
Yuichi Kanno <fksmhr077@gmail.com>
2020-12-29 10:10:14 +0900

    useReducerを使う

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers/index';
 
 const App = () => {
+  // useReducer
+  const [state, dispatch] = useReducer(reducer, []);
+  // useState
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addEvent = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'CREATE_EVENT',
+      title: title,
+      body: body,
+    });
+    setTitle('');
+    setBody('');
+  };
+
   return (
     <div className='container'>
       <h4>TODO管理</h4>
       <form>
         <div className='form-group'>
           <label htmlFor='formEventTitle'>タイトル</label>
-          <input className='form-control' id='formEventTitle' />
+          <input
+            className='form-control'
+            id='formEventTitle'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='formEventBody'>詳細</label>
-          <textarea className='form-control' id='formEventBody' />
+          <textarea
+            className='form-control'
+            id='formEventBody'
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
-        <button className='btn btn-primary'>TODOを作成する</button>
+        <button className='btn btn-primary' onClick={addEvent}>
+          TODOを作成する
+        </button>
         <button className='btn btn-danger'>全てのTODOを削除する</button>
       </form>
 

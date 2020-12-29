c69ae1c784e3c9912e00e5849c3184625b2c5f17
Yuichi Kanno <fksmhr077@gmail.com>
2020-12-29 13:52:09 +0900

    Updates

---
+++ precache-manifest.54d28ba7fa50f398194ac32513f79516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c280c2451762b6fa43f89bf31836a6b",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "85a36cc51e4d6b486033",
+    "url": "/myapp/static/css/2.263a02f0.chunk.css"
+  },
+  {
+    "revision": "85a36cc51e4d6b486033",
+    "url": "/myapp/static/js/2.88cfc011.chunk.js"
+  },
+  {
+    "revision": "abb2b700484f787f3f28",
+    "url": "/myapp/static/js/main.576a3b98.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);

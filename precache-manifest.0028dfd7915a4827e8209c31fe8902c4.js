c19c0e98b5bf49efb798e63b191ff66cbaee32f7
Yuichi Kanno <fksmhr077@gmail.com>
2020-12-29 14:02:14 +0900

    Updates

---
+++ precache-manifest.0028dfd7915a4827e8209c31fe8902c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dc98a584160272c4b8d2d112f23ffb",
+    "url": "/react-hooks-study/index.html"
+  },
+  {
+    "revision": "85a36cc51e4d6b486033",
+    "url": "/react-hooks-study/static/css/2.263a02f0.chunk.css"
+  },
+  {
+    "revision": "85a36cc51e4d6b486033",
+    "url": "/react-hooks-study/static/js/2.88cfc011.chunk.js"
+  },
+  {
+    "revision": "abb2b700484f787f3f28",
+    "url": "/react-hooks-study/static/js/main.576a3b98.chunk.js"
+  },
+  {
+    "revision": "fa32baa6083b77e326e7",
+    "url": "/react-hooks-study/static/js/runtime~main.a4622914.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.54d28ba7fa50f398194ac32513f79516.js"
+  "/react-hooks-study/precache-manifest.0028dfd7915a4827e8209c31fe8902c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-study/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
